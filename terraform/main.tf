94fa8b14d0c12910df764aaf70d0f5a1e22bed68
djeukeu <dmxchristian@gmail.com>
2025-07-25 11:19:13 +0100

    feat: terraform custom network Iaac

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,42 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
+
+data "aws_availability_zones" "az-available" {
+  state                  = "available"
+  all_availability_zones = true
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(data.aws_availability_zones.az-available.names)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.${10 + count.index}.0/24"
+  availability_zone       = data.aws_availability_zones.az-available.names[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id     = aws_vpc.vpc.id
+  depends_on = [aws_vpc.vpc]
+}
+
+resource "aws_internet_gateway_attachment" "attach_igw" {
+  internet_gateway_id = aws_internet_gateway.igw.id
+  vpc_id              = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  depends_on = [aws_internet_gateway_attachment.attach_igw]
+}
+
+resource "aws_route_table_association" "subnet_route_table_association" {
+  count          = length(aws_subnet.public_subnet)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
