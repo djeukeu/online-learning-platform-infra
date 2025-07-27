98c7e54f5e1171417cdd8f7b6a4b4be54d4ab027
djeukeu <dmxchristian@gmail.com>
2025-07-27 17:15:01 +0100

    fix: update admin policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -163,7 +163,7 @@ resource "aws_eks_access_entry" "admin_access" {
   type          = "STANDARD"
 }
 
-resource "aws_eks_access_policy_association" "admin_access_policy" {
+resource "aws_eks_access_policy_association" "admin_access_AmazonEKSAdminPolicy" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
   principal_arn = var.admin_arn
@@ -172,6 +172,17 @@ resource "aws_eks_access_policy_association" "admin_access_policy" {
     type = "cluster"
   }
 }
+
+resource "aws_eks_access_policy_association" "admin_access_AmazonEKSAdminViewPolicy" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminViewPolicy"
+  principal_arn = var.admin_arn
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
 
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
