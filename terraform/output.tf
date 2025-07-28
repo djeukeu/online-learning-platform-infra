2a8e8b6b66a604ae8c253322b8b61f3922007b67
djeukeu <dmxchristian@gmail.com>
2025-07-28 09:43:32 +0100

    feat: terraform ci

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,33 +1,33 @@
-output "eks_cluster" {
-  value = {
-    id       = aws_eks_cluster.eks_cluster.id
-    endpoint = aws_eks_cluster.eks_cluster.endpoint
-  }
-}
+# output "eks_cluster" {
+#   value = {
+#     id       = aws_eks_cluster.eks_cluster.id
+#     endpoint = aws_eks_cluster.eks_cluster.endpoint
+#   }
+# }
 
-output "auth_db" {
-  value = {
-    endpoint = module.auth_db.endpoint
-    username = module.auth_db.username
-    db_name  = module.auth_db.db_name
-    port     = module.auth_db.port
-  }
-}
+# output "auth_db" {
+#   value = {
+#     endpoint = module.auth_db.endpoint
+#     username = module.auth_db.username
+#     db_name  = module.auth_db.db_name
+#     port     = module.auth_db.port
+#   }
+# }
 
-output "payment_db" {
-  value = {
-    endpoint = module.payment_db.endpoint
-    username = module.payment_db.username
-    db_name  = module.payment_db.db_name
-    port     = module.payment_db.port
-  }
-}
+# output "payment_db" {
+#   value = {
+#     endpoint = module.payment_db.endpoint
+#     username = module.payment_db.username
+#     db_name  = module.payment_db.db_name
+#     port     = module.payment_db.port
+#   }
+# }
 
-output "course_db" {
-  value = {
-    endpoint = module.course_db.endpoint
-    username = module.course_db.username
-    db_name  = module.course_db.db_name
-    port     = module.course_db.port
-  }
-}
+# output "course_db" {
+#   value = {
+#     endpoint = module.course_db.endpoint
+#     username = module.course_db.username
+#     db_name  = module.course_db.db_name
+#     port     = module.course_db.port
+#   }
+# }
